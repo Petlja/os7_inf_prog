6be93585e5c9260f7ab58e32a42ee7365807360a
lazarbojicic <ahilej1196bc@gmail.com>
2020-03-15 16:12:49 +0100

    initial commit
    runestine init

---
+++ pavement.py
@@ -0,0 +1,52 @@
+import paver
+from paver.easy import *
+import paver.setuputils
+paver.setuputils.install_distutils_tasks()
+import os, sys
+from runestone.server import get_dburl
+from sphinxcontrib import paverutils
+import pkg_resources
+
+sys.path.append(os.getcwd())
+
+home_dir = os.getcwd()
+master_url = 'http://127.0.0.1:8000'
+master_app = 'runestone'
+serving_dir = "./build/Online_Nastava_VII"
+dest = "../../static"
+
+options(
+    sphinx = Bunch(docroot=".",),
+
+    build = Bunch(
+        builddir="./build/Online_Nastava_VII",
+        sourcedir="_sources",
+        outdir="./build/Online_Nastava_VII",
+        confdir=".",
+        project_name = "Online_Nastava_VII",
+        template_args={'course_id': 'Online_Nastava_VII',
+                       'login_required':'false',
+                       'appname':master_app,
+                       'loglevel': 0,
+                       'course_url':master_url,
+                       'use_services': 'false',
+                       'python3': 'false',
+                       'dburl': '',
+                       'default_ac_lang': 'python',
+                       'basecourse': 'Online_Nastava_VII',
+                       'jobe_server': 'http://jobe2.cosc.canterbury.ac.nz',
+                       'proxy_uri_runs': '/jobe/index.php/restapi/runs/',
+                       'proxy_uri_files': '/jobe/index.php/restapi/files/',
+                       'downloads_enabled': 'false',
+                       'enable_chatcodes': 'false'
+                        }
+    )
+)
+
+version = pkg_resources.require("runestone")[0].version
+options.build.template_args['runestone_version'] = version
+
+# If DBURL is in the environment override dburl
+options.build.template_args['dburl'] = get_dburl(outer=locals())
+
+from runestone import build  # build is called implicitly by the paver driver.
