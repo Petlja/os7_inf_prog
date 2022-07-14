9909b3028c50aab8b941679a7a263500e40bb49d
Darko Peric <darko@malopero.com>
2022-07-14 06:35:00 +0200

    Update koncentricni_krugovi.py
    Ispravljen kod za zadatak koncetrici krugovi pomocu petlje

--- _includes/koncentricni_krugovi.py
+++ _includes/koncentricni_krugovi.py
@@ -1,5 +1,6 @@
 # -*- acsection: general-init -*-
 import pygame as pg
+import pygamebg
 
 pg.init()  # inicijalizujemo rad biblioteke PyGame
 pg.display.set_caption("Koncentricni krugovi")  # otvaramo prozor
@@ -8,40 +9,19 @@ prozor = pg.display.set_mode((sirina, visina))
 
 # -*- acsection: main -*-
 
-broj_krugova = 1   # broj krugova koji se trenutno craju
-r = 10             # razlika poluprečnika susednih krugova
-dk = 1             # promena broja krugova
+# bojimo pozadinu prozora u belo
+prozor.fill(pg.Color("white"))
 
-def crtaj():
-    prozor.fill(pg.Color("white"))            # bojimo pozadinu u belo
-    (cx, cy) = (sirina // 2, visina // 2)     # centar krugova
-    for i in range(1, broj_krugova + 1):      # crtamo krugove
-        pg.draw.circle(prozor, pg.Color("red"), (cx, cy), i*r, 2)
+# centar kruga je u centru prozora - obrati pažnju na tip podataka
+centar = (sirina//2, visina//2)
 
-def novi_frejm():
-    global broj_krugova, dk
-    # ako se promenom u tekućem smeru broj krugova povećava ili
-    # smanjuje previše, menjamo smer promene
-    if broj_krugova + dk > 10 or broj_krugova + dk < 1: 
-        dk = -dk
-    broj_krugova += dk  # menjamo broj krugova za 1, u odgovarajućem smeru
+# poluprečnik se menja od 10 do 100, sa korakom 10
+for r in range(10, 101, 10):
+    # crtamo krug
+    pg.draw.circle(prozor, pg.Color("red"), centar, r, 5)
+
 
 # -*- acsection: after-main -*-
 
-kraj = False
-sat = pg.time.Clock()  # sat koji određuje broj frejmova u sekundi
-while not kraj:
-    # crtamo i ažuriramo sadržaj prozora
-    crtaj()
-    pg.display.update()
-
-    # proveravamo da li je korisnik isključio prozor
-    for dogadjaj in pg.event.get():
-        if dogadjaj.type == pg.QUIT:
-            kraj = True
-
-    # pauziramo do sledeceg frejma
-    sat.tick(10)
-    novi_frejm()
-
-pg.quit() # isključujemo rad biblioteke PyGame
+# prikazujemo prozor i cekamo da ga korisnik iskljuci
+pygamebg.wait_loop()
